08fff81a75ba028792f009b9363da555c139aed6
alexander gubernikoff <agubernikoff@gmail.com>
2022-02-09 01:06:48 -0500

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
-  
-}
+  form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const inputValue = document.querySelector("#searchByID").value;
+    console.log(inputValue);
+    fetch(`http://localhost:3000/movies/${inputValue}`)
+      .then((resp) => resp.json())
+      .then((data) => renderMovie(data));
+    function renderMovie(movie) {
+      console.log(movie.title, movie.summary);
+      document.querySelector("h4").innerText = movie.title;
+      document.querySelector("h4").nextElementSibling.innerText = movie.summary;
+    }
+  });
+};
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
